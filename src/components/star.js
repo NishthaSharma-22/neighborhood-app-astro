900bc3993b8bc06dcb270a7fb5438e2cbe7509ed
NishthaSharma-22 <nishthapinjore@gmail.com>
2025-05-08 14:41:55 +0530

    added few new animations

---
+++ src/components/star.js
@@ -0,0 +1,11 @@
+const particleCount = 100;
+const nebula = document.getElementById('particles');
+
+for(let i = 0; i<particleCount; i++){
+    const p = document.createElement('div');
+    p.classList.add('particle');
+    const size = Math.random()*4+2;
+    
+
+    
+}
